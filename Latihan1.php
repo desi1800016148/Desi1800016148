52299f40b8694ba3bfd19013ad00c2c15c205639
desi1800016148 <desi1800016148@webmail.uad.ac.id>
2020-06-14 20:34:46 +0700

    first commit

---
+++ Latihan1.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+<title>output</title>
+</head>
+
+<table border="1" align="left">
+<tr>
+     <td width="300">NIM</td>
+     <td width="15">:</td>
+     <td width="300"> <?php echo $_POST["NIM"] ?></td>
+</tr>
+<tr>
+     <td>NAMA</td>
+     <td>:</td>
+     <td><?php echo $_POST["NAMA"]?></td>
+</tr>
+<tr>
+     <td>Jenis Kelamin</td>
+     <td>:</td>
+     <td><?php echo $_POST["KELAMIN"]?></td>
+</tr>
+<tr>
+     <td>Program Studi</td>
+     <td>:</td>
+     <td><?php echo $_POST["PRODI"]?></td>
+</tr>
+<tr>
+     <td align="center" colspan="3"><a href="Latihan1.html">BACK </a></td>
+</tr>
+</table>
+</body>
+</html>
---
+++ Latihan2.php
@@ -0,0 +1,41 @@
+<html>
+<body>
+
+<table border="1" align="left">
+<tr>
+     <td width="300">NIM</td>
+     <td width="15">:</td>
+     <td width="300"> <?php echo $_POST["NIM"] ?></td>
+</tr>
+<tr>
+     <td>NAMA</td>
+     <td>:</td>
+     <td><?php echo $_POST["NAMA"]?></td>
+</tr>
+<tr>
+     <td>JENIS KELAMIN</td>
+     <td>:</td>
+     <td><?php echo $_POST["KELAMIN"]?></td>
+</tr>
+<tr>
+     <td>ALAMAT</td>
+     <td>:</td>
+     <td><?php echo $_POST["ALAMAT"]?></td>
+</tr>
+<tr>
+     <td>PROGRAM STUDI</td>
+     <td>:</td>
+     <td><?php echo $_POST["PRODI"]?></td>
+</tr>
+<tr>
+     <td>BAHASA PEMROGRAMAN YANG DIKUASAI</td>
+     <td>:</td>
+     <td><?php echo $_POST["PEMROGRAMAN"]?></td>
+     
+</tr>
+<tr>
+     <td align="center" colspan="3"><a href="form.html">BACK </a></td>
+</tr>
+</table>
+</body>
+</html>
